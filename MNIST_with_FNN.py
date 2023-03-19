5078668b07cb24e4b3880eaa1dea0296791e4339
kukkadapudheeraj <kukkadapudheeraj@gmail.com>
2023-03-18 20:59:20 -0400

    Update MNIST_with_FNN.py

--- MNIST_with_FNN.py
+++ MNIST_with_FNN.py
@@ -1,78 +1,74 @@
 import torch
 import torch.nn as nn
 import torch.optim as optim
+from torch.utils.data import DataLoader
+from torchvision.datasets import MNIST
+from torchvision.transforms import ToTensor
 import matplotlib.pyplot as plt
 
-# Define the neural network architecture
-class Net(nn.Module):
+
+# Define the FNN model
+class FNN(nn.Module):
     def __init__(self):
-        super(Net, self).__init__()
-        self.fc1 = nn.Linear(10, 5)
-        self.fc2 = nn.Linear(5, 1)
+        super(FNN, self).__init__()
+        self.flatten = nn.Flatten()
+        self.fc1 = nn.Linear(784, 128)
+        self.relu1 = nn.ReLU()
+        self.fc2 = nn.Linear(128, 10)
 
     def forward(self, x):
-        x = torch.relu(self.fc1(x))
+        x = self.flatten(x)
+        x = self.fc1(x)
+        x = self.relu1(x)
         x = self.fc2(x)
         return x
 
-# Define the training function
-def train(net, train_loader,test_loader, optimizer, criterion, epochs):
-    train_losses = []
-    test_losses = []
-    for epoch in range(epochs):
-        running_loss = 0.0
-        for i, data in enumerate(train_loader, 0):
-            inputs, labels = data
-            optimizer.zero_grad()
-            outputs = net(inputs)
-            loss = criterion(outputs, labels)
-            loss.backward()
-            optimizer.step()
-            running_loss += loss.item()
-        train_losses.append(running_loss/len(train_loader))
-        test_loss = test(net, test_loader, criterion)
-        test_losses.append(test_loss)
-        print('Epoch %d: Training Loss: %.3f, Test Loss: %.3f' % (epoch+1, train_losses[-1], test_loss))
-    return train_losses, test_losses
 
-# Define the test function
-def test(net, test_loader, criterion):
-    running_loss = 0.0
-    with torch.no_grad():
-        for data in test_loader:
-            inputs, labels = data
-            outputs = net(inputs)
-            loss = criterion(outputs, labels)
-            running_loss += loss.item()
-    return running_loss/len(test_loader)
+# Create data loaders
+train_data = MNIST(root='./data', train=True, transform=ToTensor(), download=True)
+test_data = MNIST(root='./data', train=False, transform=ToTensor(), download=True)
+train_loader = DataLoader(train_data, batch_size=64, shuffle=True)
+test_loader = DataLoader(test_data, batch_size=64, shuffle=False)
 
-# Define the main function
-def main():
-    # Load the data
-    train_data = torch.randn(100, 10)
-    train_labels = torch.randn(100, 1)
-    test_data = torch.randn(50, 10)
-    test_labels = torch.randn(50, 1)
-    train_dataset = torch.utils.data.TensorDataset(train_data, train_labels)
-    test_dataset = torch.utils.data.TensorDataset(test_data, test_labels)
-    train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=10, shuffle=True)
-    test_loader = torch.utils.data.DataLoader(test_dataset, batch_size=10, shuffle=False)
+# Initialize the model and the optimizer
+model = FNN()
+optimizer = optim.Adam(model.parameters(), lr=0.001)
 
-    # Initialize the network, optimizer, and loss function
-    net = Net()
-    optimizer = optim.SGD(net.parameters(), lr=0.1)
-    criterion = nn.MSELoss()
+# Define the loss function
+criterion = nn.CrossEntropyLoss()
 
-    # Train the network and get the training and test losses
-    train_losses, test_losses = train(net, train_loader,test_loader, optimizer, criterion, epochs=100)
+# Train the model
+train_losses = []
+test_losses = []
+num_epochs = 10
+for epoch in range(num_epochs):
+    train_loss = 0.0
+    for i, (images, labels) in enumerate(train_loader):
+        optimizer.zero_grad()
+        outputs = model(images)
+        loss = criterion(outputs, labels)
+        loss.backward()
+        optimizer.step()
+        train_loss += loss.item()
 
-    # Plot the training and test losses
-    plt.plot(range(len(train_losses)), train_losses, label='Training Loss')
-    plt.plot(range(len(test_losses)), test_losses, label='Test Loss')
-    plt.xlabel('Iterations')
-    plt.ylabel('Loss')
-    plt.legend()
-    plt.show()
+    train_loss /= len(train_loader)
+    train_losses.append(train_loss)
 
-if __name__ == '__main__':
-    main()
+    test_loss = 0.0
+    for i, (images, labels) in enumerate(test_loader):
+        outputs = model(images)
+        loss = criterion(outputs, labels)
+        test_loss += loss.item()
+
+    test_loss /= len(test_loader)
+    test_losses.append(test_loss)
+
+    print(f'Epoch {epoch+1}/{num_epochs}, Train Loss: {train_loss:.4f}, Test Loss: {test_loss:.4f}')
+
+# Plot the training and test losses
+plt.plot(train_losses, label='Train Loss')
+plt.plot(test_losses, label='Test Loss')
+plt.xlabel('Iterations')
+plt.ylabel('Loss')
+plt.legend()
+plt.show()
